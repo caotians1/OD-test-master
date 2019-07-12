ca64ce6a84b16e323dc00407db5486bad6e795ca
jeanlancel <tian-shi11@hotmail.com>
2019-07-11 20:01:27 -0400

    ADDing residual AE experiments

---
+++ setup/NIHTrainAERes.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import os,sys,inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+from utils.args import args
+
+import categories.ae_setup as AESetup
+from models.autoencoders import Generic_VAE, Generic_AE, Residual_AE
+from datasets.NIH_Chest import NIHChestBinaryTrainSplit
+
+if __name__ == "__main__":
+    dataset = NIHChestBinaryTrainSplit(root_path=os.path.join(args.root_path, "NIHCC"), binary=True, expand_channels=False, downsample=64)
+    model = Residual_AE(dims=(1, 64, 64))
+    AESetup.train_autoencoder(args, model=model, dataset=dataset.get_D1_train(), BCE_Loss=True)
+
---
+++ train_NIHRESAE.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=rpp-bengioy
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G
+#SBATCH -c 8
+#SBATCH -n 1
+#SBATCH --tmp=256G
+#SBATCH --time=18:00:00
+
+module load python/3.6
+mkdir -p $SLURM_TMPDIR/env/temp
+mkdir -p $SLURM_TMPDIR/data
+
+cp -r ~/Venv/temp/* $SLURM_TMPDIR/env/temp
+cp -r  ~/projects/rpp-bengioy/caotians/data/* $SLURM_TMPDIR/data
+
+mkdir -p $SLURM_TMPDIR/data/NIHCC/images_224
+tar -xzf $SLURM_TMPDIR/data/NIHCC/images_224.tar.gz -C $SLURM_TMPDIR/data/NIHCC/images_224 --strip-components=1
+tar -xzf $SLURM_TMPDIR/data/MURA/images_224.tar.gz -C $SLURM_TMPDIR/data/MURA
+tar -xf $SLURM_TMPDIR/data/PADChest/images-64.tar -C $SLURM_TMPDIR/data/PADChest
+
+source $SLURM_TMPDIR/env/temp/bin/activate
+python setup_datasets.py
+ln -sf $SLURM_TMPDIR/data workspace/datasets-$SLURM_JOBID
+python setup/NIHTrainAERES.py --root_path=workspace/datasets-$SLURM_JOBID --exp="NIHAERES" --batch-size=64 --no-visualize --save --workers=8
