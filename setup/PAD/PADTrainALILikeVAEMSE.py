335d7abc4d5dc4f646aa50dbcbbe7846bf744370
jeanlancel <tian-shi11@hotmail.com>
2019-08-24 17:04:48 -0400

    fixing path

--- setup/PAD/PADTrainALILikeVAEMSE.py
+++ setup/PAD/PADTrainALILikeVAEMSE.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import os,sys,inspect
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(os.path.dirname(currentdir))
 sys.path.insert(0,parentdir)
 
 from utils.args import args
