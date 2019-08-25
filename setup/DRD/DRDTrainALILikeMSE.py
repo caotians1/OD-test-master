dc6fc00a386ee621e88a0f1812074f0979ca517e
jeanlancel <tian-shi11@hotmail.com>
2019-08-25 14:03:15 -0400

    DRD AEs scripts

---
+++ setup/DRD/DRDTrainALILikeMSE.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import os,sys,inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(os.path.dirname(currentdir))
+sys.path.insert(0,parentdir)
+
+from utils.args import args
+
+import setup.categories.ae_setup as AESetup
+from models.autoencoders import *
+from datasets.DRD import DRD
+
+if __name__ == "__main__":
+    dataset = DRD(root_path=os.path.join(args.root_path, "diabetic-retinopathy-detection"), downsample=64)
+    model = ALILikeAE(dims=(3, 64, 64))
+    AESetup.train_autoencoder(args, model=model, dataset=dataset.get_D1_train(), BCE_Loss=False)
+
--- train_DRDAEs.sh
+++ train_DRDAEs.sh
@@ -2,21 +2,36 @@
 #SBATCH --account=rpp-bengioy
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G
-#SBATCH -c 8
-#SBATCH -n 1
+#SBATCH --cpus-per-task=8
 #SBATCH --tmp=256G
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
+#SBATCH -a 0-8
 
-module load python/3.6
-mkdir -p $SLURM_TMPDIR/env/temp
-mkdir -p $SLURM_TMPDIR/data
+PARRAY1=(DRDTrainAEBCE.py DRDTrainAEMSE.py DRDTrainVAEBCE.py DRDTrainVAEMSE.py DRDTrainALILikeBCE.py DRDTrainALILikeMSE.py DRDTrainALILikeVAEBCE.py DRDTrainALILikeVAEMSE.py)
+PARRAY2=(DRDAEBCE DRDAEMSE DRDVAEBCE DRDVAEMSE DRDALIBCE DRDALIMSE DRDALIVAEBCE DRDALIVAEMSE)
 
-cp -r ~/Venv/temp/* $SLURM_TMPDIR/env/temp
-cp -r  ~/projects/rpp-bengioy/caotians/data/diabetic-retinopathy-detection $SLURM_TMPDIR/data
+for i in {0..8}
+    do
+        let task_id=$i
+        # printf $task_id"\n" # comment in for testing
 
-tar -xzf $SLURM_TMPDIR/data/diabetic-retinopathy-detection/images_224.tar.gz -C $SLURM_TMPDIR/data/diabetic-retinopathy-detection
+        if [ "$task_id" -eq "$SLURM_ARRAY_TASK_ID" ]
+        then
+            p1=${PARRAY1[$i]}
+            p2=${PARRAY2[$i]}
+            module load python/3.6
+            mkdir -p $SLURM_TMPDIR/env/temp
+            mkdir -p $SLURM_TMPDIR/data
 
-source $SLURM_TMPDIR/env/temp/bin/activate
-ln -sf $SLURM_TMPDIR/data workspace/datasets-$SLURM_JOBID
-export DISABLE_TQDM="True"
-python setup/DRDTrain.py --root_path=workspace/datasets-$SLURM_JOBID --exp="DRDDense" --batch-size=64 --no-visualize --save --workers=8
+            cp -r ~/Venv/temp/* $SLURM_TMPDIR/env/temp
+            cp -r  ~/projects/rpp-bengioy/caotians/data/diabetic-retinopathy-detection $SLURM_TMPDIR/data
+
+            tar -xzf $SLURM_TMPDIR/data/diabetic-retinopathy-detection/images_224.tar.gz -C $SLURM_TMPDIR/data/diabetic-retinopathy-detection
+
+            source $SLURM_TMPDIR/env/temp/bin/activate
+            ln -sf $SLURM_TMPDIR/data workspace/datasets-$SLURM_JOBID
+            export DISABLE_TQDM="True"
+
+            python setup/DRD/$p1 --root_path=workspace/datasets-$SLURM_JOBID --exp=$p2 --batch-size=64 --no-visualize --save --workers=8
+         fi
+    done
