d5fca195e1d5dbc834dc61655f4d9616df7a75d7
jeanlancel <tian-shi11@hotmail.com>
2019-10-10 19:04:38 -0400

    fixing path issue for NIH training scripts

--- setup/NIH/NIHTrain_binary.py
+++ setup/NIH/NIHTrain_binary.py
@@ -3,7 +3,7 @@ import os,sys,inspect
 from termcolor import colored
 import torch
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(os.path.dirname(currentdir))
 sys.path.insert(0,parentdir)
 
 import models as Models
